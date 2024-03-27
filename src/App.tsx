4ce48460c14754b7fe3acd0ee5e62d3247de8e76
swh00tw <a6140000@gmail.com>
2024-03-26 23:55:41 -0400

    feat: swithc to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import type { Component } from "solid-js";
-import { cn } from "./utils/cn";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: Component = () => {
-  return <div class={cn("text-red-800")}>1123</div>;
-};
+function App() {
+  const [count, setCount] = useState(0)
 
-export default App;
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
