98ea684c6c211b9c58cc05474b13dff5de45f220
swh00tw <a6140000@gmail.com>
2024-03-26 23:44:39 -0400

    feat: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import type { Component } from "solid-js";
+import { cn } from "./utils/cn";
 
 const App: Component = () => {
-  return <div>123</div>;
+  return <div class={cn("text-red-800")}>1123</div>;
 };
 
 export default App;
